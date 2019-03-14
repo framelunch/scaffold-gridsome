a555dab59e35cf976ad6ded80c6985aacfeedff9
kaave <abe@framelunch.jp>
2019-03-14 11:40:22 +0900

    cms: add configs

--- src/main.js
+++ src/main.js
@@ -7,4 +7,15 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+
+  head.script.push({ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' });
+
+  if (isClient && window.netlifyIdentity) {
+    window.netlifyIdentity.on('init', user => {
+      if (!user) {
+        // eslint-disable-next-line no-return-assign
+        window.netlifyIdentity.on('login', () => (document.location.href = '/admin/'));
+      }
+    });
+  }
 }
